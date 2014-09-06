fd22f61f9d767fabbb0b14f27bbd2174e0291692
rkJun <juntai81@gmail.com>
2014-09-06 13:24:50 +0900

    add user's bio field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140828144424) do
+ActiveRecord::Schema.define(version: 20140906041633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20140828144424) do
     t.string   "image"
     t.string   "role"
     t.string   "slug"
+    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
