c638b43ba04e0bdbb58501aa9b962f4dc1eea154
rkJun <juntai81@gmail.com>
2014-09-07 21:31:46 +0900

    add user's self intro

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ Bulletin.create! title: 'news', description: '새소식'
 Bulletin.create! title: 'knowledge', description: '기술공유', post_type: 'blog'
 Bulletin.create! title: 'devlife', description: '개발자의 삶', post_type: 'blog'
 Bulletin.create! title: 'untitled', description: '잡담', post_type: 'blog'
+Bulletin.create! title: 'selfintro', description: '자기소개'
 
 # User.create! id: 2, provider: 'facebook', uid: '999999999', name: 'FirstName LastName'
 #, email: 'email@address.com', nickname: '빵줘맨', image: 'http://image.com/jpg'
