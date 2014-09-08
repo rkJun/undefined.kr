e18f639303acf5aa10dcc75e1be5f4ec9d693f19
rkJun <juntai81@gmail.com>
2014-09-08 12:53:50 +0900

    add post's validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,11 @@ class Post < ActiveRecord::Base
   belongs_to :bulletin
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
+
+  validates_length_of :title, :minimum => 10
+  validates_length_of :title, :minimum => 10
 end
