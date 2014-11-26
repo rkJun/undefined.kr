b22fe6e104945a5de89793496a45f4cb31e08021
rkJun <juntai81@gmail.com>
2014-11-26 18:55:33 +0900

    add ios top free apps

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -3,18 +3,32 @@ require 'open-uri'
 
 class AppsController < ApplicationController
   def index
-  	countries = [ 'kr', 'us', 'jp', 'uk', 'cn', 'hk' ]
-  	feed_types = [ 'topmacapps', 'topfreemacapps', 'toppaidmacapps' ]
-  	limit = 100
+  	countries = [ 'kr', 'us', 'jp'] #, 'uk', 'cn', 'hk' ]
+  	# feed_types = [ 'topmacapps', 'topfreemacapps', 'toppaidmacapps' ]
+  	feed_types = [ 'topfreeapplications', 'toppaidapplications']
+    limit = 100
 
   	url_prefix = 'https://itunes.apple.com/'
   	date = Time.new.strftime("%Y-%m-%d")
 
   	country = "kr"
 
-  	# https://itunes.apple.com/kr/rss/topfreemacapps/limit=10/xml
-  	url = "#{url_prefix}/#{country}/rss/#{feed_types[1]}/limit=#{limit}/xml"
+    # Korea free top ios all
+    #https://itunes.apple.com/kr/rss/topfreeapplications/limit=100/xml
+    # Japan Paid top ios all
+    # https://itunes.apple.com/jp/rss/topfreeapplications/limit=100/xml
 
-  	@rss = SimpleRSS.parse open(url)
+
+  	# https://itunes.apple.com/kr/rss/topfreemacapps/limit=10/xml
+  	# url = "#{url_prefix}#{country}/rss/#{feed_types[1]}/limit=#{limit}/xml"
+
+    url_free_kr = 'https://itunes.apple.com/kr/rss/topfreeapplications/limit=100/xml'
+    url_free_jp = 'https://itunes.apple.com/jp/rss/topfreeapplications/limit=100/xml'
+    url_free_us = 'https://itunes.apple.com/us/rss/topfreeapplications/limit=100/xml'
+
+    @rss_free_kr = SimpleRSS.parse open(url_free_kr)
+    @rss_free_jp = SimpleRSS.parse open(url_free_jp)
+    @rss_free_us = SimpleRSS.parse open(url_free_us)
   end
 end
+
