73d153e051131524d780240565de5d9128341db4
Juntai Park <juntai81@gmail.com>
2015-06-05 01:49:00 +0900

    Add Action mailer config   - Don't forget add gmail account information in .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,19 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Action mailer
+  config.action_mailer.default_url_options = { host: 'undefined.kr' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            ENV['GMAIL_ID'],
+      password:             ENV['GMAIL_PW'],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
